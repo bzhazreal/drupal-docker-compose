404eefe93aca47a69b3cd0ba8ee88551eef99161
bzhazreal <stanislas.david@protonmail.com>
2020-11-27 22:34:46 +0100

    Improve local env build make command (env-build && env-purge) && remove web directory

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Release 0.0.2
+- Improve local env build make command (env-build && env-purge) && remove web directory
+
+# Release 0.0.1
+- Add docker-compose default stack
+- Add docker default containers
+- Add default confirguration
+- Add base files
--- bin/scripts/build_env.sh
+++ bin/scripts/build_env.sh
@@ -5,35 +5,49 @@ export ROOT_DIRECTORY="$(dirname "$(dirname "${SCRIPT_DIRECTORY}")")"
 
 source "${SCRIPT_DIRECTORY}"/_lib.sh
 
-###
-#
-# Env file check
-# - Ignore if env file already exist.
-# - Create env file from env.example if not exist
-#
-###
-if [ ! -f "${ROOT_DIRECTORY}"/.env ];then
+if [[ "$@" == "--build" ]];then
+
+  print_title "Build environment"
   
-  print_info "Environment file not found, create new one from env.example"
-  
-  if [ ! -f "${ROOT_DIRECTORY}"/env.example ];then
-    print_error "env.example file is missing in ${ROOT_DIRECTORY}"  
+  if [ ! -f "${ROOT_DIRECTORY}"/.env ];then
+    print_info "Environment file not found, create new one from env.example"
+    [ ! -f "${ROOT_DIRECTORY}"/env.example ] && break_message "env.example file is missing in ${ROOT_DIRECTORY}"
+    cp "${ROOT_DIRECTORY}"/env.example .env
+    [ -f "${ROOT_DIRECTORY}"/.env ] && print_success "Environment file has been generate" || break_message "Fail to generate .env file"
   fi
 
-  cp "${ROOT_DIRECTORY}"/env.example .env
-
-  if [ -f "${ROOT_DIRECTORY}"/.env ];then
-    print_success "Environment file has been generate"
-  else
-    print_error "Fail to generate .env file"
+  if [ ! -d ${ROOT_DIRECTORY}/src ];then
+    print_info "Src directory not found, create one"
+    mkdir ${ROOT_DIRECTORY}/src
+    [ -d ${ROOT_DIRECTORY}/src ] && print_success "Src directory has been created" || break_message "Fail to create src directory"
   fi
+
+  cd "${ROOT_DIRECTORY}"
+  docker-compose up --build -d
+
+elif [[ "$@" == "--purge" ]];then
+
+  print_title "Purge environment"
+
+  while true; do
+    read -r -p "$(echo -e "${red}${bold}Warning : This command will reset all env, including src/ directory and remove all containers. Are you sure (n/y) ?${reset}")" RESPONSE
+    if [[ "${RESPONSE}" == "n" || "${RESPONSE}" == "N" ]];then
+      break_message "Abort project clean"
+    elif [[ "${RESPONSE}" == "y" || "${RESPONSE}" == "Y" ]];then
+      break;
+    else
+      continue
+    fi
+  done
+
+  docker-compose down --rmi all --remove-orphans -v || break_message "Fail to purge containers"
+  print_success "Containers delete with success"
+
+  [ -f "${ROOT_DIRECTORY}"/.env ] && rm -f "${ROOT_DIRECTORY}"/.env || print_info "Env file not found"
+  [ -d "${ROOT_DIRECTORY}"/src ]  && rm -rf "${ROOT_DIRECTORY}"/src || print_info "Directory src not found"
+  if [[ -f "${ROOT_DIRECTORY}"/.env && -d "${ROOT_DIRECTORY}"/src ]];then
+    break_message "Failed to delete files"
+  fi
+  print_success "Files delete with success"
 
 fi
-
-###
-#
-# Execute docker-compose
-#
-###
-cd "${ROOT_DIRECTORY}"
-docker-compose up --build -d
